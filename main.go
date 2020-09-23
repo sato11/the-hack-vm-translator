11afa79fd9c9d04cfdea34017c4e5f9776bebc7b
Junichi Sato <22004610+sato11@users.noreply.github.com>
2020-09-23 16:33:12 +0900

    define basics for the main program

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// ExitCodeOK and ExitCodeError represent respectively a status code.
+const (
+	ExitCodeOK int = iota
+	ExitCodeError
+)
+
+func readFile(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(string(b))
+	return nil
+}
+
+// main reads single file when argument is vm file.
+// otherwise recursively searches for vm files under the given path.
+func main() {
+	path := os.Args[1]
+	if filepath.Ext(path) == ".vm" {
+		err := readFile(path)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(ExitCodeError)
+		}
+	} else {
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if filepath.Ext(path) == ".vm" {
+				err := readFile(path)
+				if err != nil {
+					fmt.Println(err.Error())
+					os.Exit(ExitCodeError)
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			os.Exit(ExitCodeError)
+		}
+	}
+	os.Exit(ExitCodeOK)
+}
